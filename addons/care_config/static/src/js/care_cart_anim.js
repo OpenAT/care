72f93bceee60ff98ee8f8fb69dddf5c59ec738e6
Alexander Koch <alex@blendpunkt.at>
2016-03-25 13:21:15 +0100

    [FIX] form submit

--- addons/care_config/static/src/js/care_cart_anim.js
+++ addons/care_config/static/src/js/care_cart_anim.js
@@ -60,12 +60,13 @@ $(document).ready( function() {
             }
         }
 
+        var $form = $(this).find('form');
 
         $move.animate({path : new $.path.bezier(bezier_params)}, 2000, function() {
             $( this ).remove();
             $move_temp.show();
 
-            $('.oe_product form').submit();
+            $form.submit();
 
         }).animateRotate(180, 1750, 250);
     });
