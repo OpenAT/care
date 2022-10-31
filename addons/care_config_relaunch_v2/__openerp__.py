1f044123ca6ffd75f8f292259c05e2191c9df7e8
Martin Kaip <16665420+kaipm@users.noreply.github.com>
2022-10-31 14:32:02 +0100

    [ADD] new addon for care website relaunch v2

---
+++ addons/care_config_relaunch_v2/__openerp__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+##############################################################################
+
+{
+    'name': 'care_config_relaunch_v2',
+    'summary': """Design adjustments for the second website relaunch""",
+    'description': """Design adjustments for the second website relaunch""",
+    'author': 'DataDialog',
+    'version': '0.1',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'care_config',
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+}
